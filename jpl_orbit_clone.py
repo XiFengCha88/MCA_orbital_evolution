2e173d4814aba684044ba51399199403999898a1
XiFengCha88 <145662941+XiFengCha88@users.noreply.github.com>
2025-04-23 14:59:31 +0800

    Add files via upload

--- jpl_orbit_clone.py
+++ jpl_orbit_clone.py
@@ -78,11 +78,11 @@ def clone_data(mca_id, num = 100):
         gaussian_evs    = cm_eigenvectors * gaussian_random
         clone_e.append(float(init_e + gaussian_evs[0]))
         clone_a.append(float((init_a + gaussian_evs[1])))
-        clone_i.append(float(init_i + gaussian_evs[5] / (180/n.pi)))
-        clone_Ω.append(float(init_Ω + gaussian_evs[3] / (180/n.pi)))
-        clone_ω.append(float(init_ω + gaussian_evs[4] / (180/n.pi)))
+        clone_i.append(float((init_i + gaussian_evs[5]) / (180/n.pi)))
+        clone_Ω.append(float((init_Ω + gaussian_evs[3]) / (180/n.pi)))
+        clone_ω.append(float((init_ω + gaussian_evs[4]) / (180/n.pi)))
     
-    # return the result
+    # return the results
     return [clone_a, clone_e, clone_i, clone_Ω, clone_ω]
     
 # object's position and velocity
@@ -101,6 +101,7 @@ jd = 2453311.5
 
 # record positional data
 resu_dir  = "Orbital_Result"
+caseid = "case" + str(6)
 
 """
 ========================================================================================================================================================
@@ -182,7 +183,7 @@ def rebound_simulation(integrate_dt, step, max_number, clone_num = 100):
                             
                     sim = None
                     
-                with open(f"/home/xi-feng/NCU/Meeting/sim_orbital_element_jd{jd}.txt", "wt", encoding = "utf-8") as rec_oe:
+                with open(f"/home/xi-feng/NCU/Meeting/clone{clone_num}_sim_orbital_element_jd{jd}_{caseid}.txt", "wt", encoding = "utf-8") as rec_oe:
                     orbital_element = {}
                     orbital_element["name"] = obj_v
                     orbital_element["element"] = {"t": [],"a": [], "e": [], "i": [], "Ω": [], "ω": []}
@@ -211,30 +212,83 @@ def plot_orbital_element(textfile):
     
     evolution_time = [dt * step * t / 365.242199 for t in range(max_points)]
     semimajor_axis = rawfile["a"]
-    #eccentricity   = rawfile["e"]
-    #inclination    = rawfile["i"]
-    #longti_node    = rawfile["Ω"]
-    #arg_perihelion = rawfile["ω"]
+    eccentricity   = rawfile["e"]
+    inclination    = rawfile["i"]
+    longti_node    = rawfile["Ω"]
+    arg_perihelion = rawfile["ω"]
     
-    fig = Mplot.figure(figsize = (10, 5))
-    #axa, axe, axi, axOm, axom = fig.subplots(5, 1)
-    axa = fig.subplots() 
-    Mplot.title(f"100 clones of {obj_v}'s semimajor axis in orbital evolution")
+    fig = Mplot.figure(figsize = (10, 10))
+    axa, axe, axOm, axom, axi = fig.subplots(5, 1)
+    #axa, axe = fig.subplots(2, 1)
+    axa.set_title(f"10 clones of {obj_v}'s orbital evolution")
     
-    axa.set_xlabel("Evolution time (yr)")
+    #axa.set_ylim(0.1, 200)
+    axi.set_xlabel("Evolution time (yr)")
     axa.set_ylabel("a (AU)")
-    axa.set_yscale("log")
+    axe.set_ylabel("e")
+    axi.set_ylabel("i (deg)")
+    axOm.set_ylabel("q (AU)")
+    axom.set_ylabel("Q (AU)")
+    #axa.set_yscale("log")
     
-    for cp in range(1, 100):
-        axa.scatter(evolution_time, semimajor_axis[cp], s = 4, c = "black")
-    axa.scatter(evolution_time, semimajor_axis[0], s = 4, c = "orange", label = "original data")
+    perihelion = [[semimajor_axis[cp][i] * (1 - eccentricity[cp][i]) for i in range(len(semimajor_axis[cp]))] for cp in range(0, 100)]
+    aphelion   = [[semimajor_axis[cp][i] * (1 + eccentricity[cp][i]) for i in range(len(semimajor_axis[cp]))] for cp in range(0, 100)]
+    
+    # saved case
+    """ 
+    === 433 Eros ===
+    case1: [17, 22, 25, 37, 57, 65, 68, 72, 80, 93]
+    case2: [12, 20, 23, 25, 35, 43, 44, 57, 96, 99]
+    case4: [9, 17, 30, 41, 45, 57, 64, 69, 89, 94]
+    case5: [1, 2, 9, 29, 34, 37, 61, 74, 78, 88]
+    case6: [3, 8, 18, 34, 39, 43, 64, 69, 70, 94]
+    """
+    datacase = [3, 8, 18, 34, 39, 43, 64, 69, 70, 94]
+    
+    for cp in datacase:
+        print("case", cp, [min(semimajor_axis[cp]), max(semimajor_axis[cp])])
+        axa.plot(evolution_time, semimajor_axis[cp], linewidth = 1, alpha = 0.75)
+        axe.plot(evolution_time, eccentricity[cp], linewidth = 1, alpha = 0.75)
+        axOm.plot(evolution_time, perihelion[cp], linewidth = 1, alpha = 0.75)
+        axom.plot(evolution_time, aphelion[cp], linewidth = 1, alpha = 0.75)
+        axi.plot(evolution_time, inclination[cp], linewidth = 1, alpha = 0.75)
+        
+    axa.plot(evolution_time, semimajor_axis[0], linestyle = "-", color = "black", linewidth = 0.75, alpha = 0.7, label = "original data")
+    axe.plot(evolution_time, eccentricity[0], linestyle = "-", color = "black", linewidth = 0.75, alpha = 0.7)
+    axi.plot(evolution_time, inclination[0], linestyle = "-", color = "black", linewidth = 0.75, alpha = 0.7)
+    axOm.plot(evolution_time, perihelion[0], linestyle = "-", color = "black", linewidth = 0.75, alpha = 0.7)
+    axom.plot(evolution_time, aphelion[0], linestyle = "-", color = "black",  linewidth = 0.75, alpha = 0.7)
     axa.legend()
-    Mplot.savefig(f"clone_para_a_jd{jd}.png")
+    Mplot.savefig(f"clone_para_a_jd{jd}_{caseid}.png")
+    Mplot.close()
     
+    #plot a-i relation
+    fig_ai = Mplot.figure(figsize = (10, 10))
+    ax_ai = fig_ai.subplots()
+    ax_ai.set_title(f"a-i relation of {obj_v}'s orbital evolution with 10 clones") 
+    ax_ai.set_xlabel("a (AU)")
+    ax_ai.set_ylabel("i (deg)")
+    
+    for cp in datacase:
+        quartor = int(len(semimajor_axis[cp]) / 4)
+        ax_ai.scatter([semimajor_axis[cp][i] for i in range(quartor)], [inclination[cp][i] for i in range(quartor)],
+                      s = 0.5, c = "blue")
+        ax_ai.scatter([semimajor_axis[cp][i] for i in range(quartor, 2 * quartor)], [inclination[cp][i] for i in range(quartor, 2 * quartor)],
+                      s = 0.5, c = "red")
+        ax_ai.scatter([semimajor_axis[cp][i] for i in range(2 * quartor, 3 * quartor)], [inclination[cp][i] for i in range(2 * quartor, 3 * quartor)],
+                      s = 0.5, c = "orange") 
+        ax_ai.scatter([semimajor_axis[cp][i] for i in range(3 * quartor, 4 * quartor)], [inclination[cp][i] for i in range(3 * quartor, 4 * quartor)],
+                      s = 0.5, c = "green")     
+    ax_ai.legend(["0 ~ τ/4", "τ/4 ~ τ/2", "τ/2 ~ 3τ/4", "3τ/4 ~ τ"])   
+    Mplot.savefig(f"paraAvsI_jd{jd}_{caseid}.png")
+    Mplot.close()
+
+# classification  
+
 
 # main operator
-rebound_simulation(dt, step, max_points)
-plot_orbital_element(f"/home/xi-feng/NCU/Meeting/sim_orbital_element_jd{jd}.txt")
+#rebound_simulation(dt, step, max_points)
+plot_orbital_element(f"/home/xi-feng/NCU/Meeting/clone100_sim_orbital_element_jd{jd}_{caseid}.txt")
 
 
 
--- mercury6_obj.py
+++ mercury6_obj.py
@@ -1,14 +1,22 @@
-import os
+import os, sys, glob
 import matplotlib.pyplot as Mplot
 
 """
 variables
 """
-
-dirpath = "/home/xi-feng/NCU/Meeting/MERCURY/mercury6_smirik/"
-obj = "1999_US3"
+raw_path = os.path.realpath(sys.argv[0])
+raw_direc = os.path.dirname(raw_path)
+work_direc = "MERCURY"
 classfile = ".aei"
-jd = 2460800.5
+
+aei_classfile = os.path.join(raw_direc + f"/{work_direc}", "**", f"*{classfile}")
+aei_glob      = glob.glob(aei_classfile, recursive = True)
+aei_file_list = sorted([i if "000" in i else '' for i in aei_glob])
+
+#dirpath = "/home/xi-feng/NCU/Meeting/MERCURY/mercury6_smirik/"
+#dirpath = "/home/xi-feng/NCU/Meeting/Orbital_Result/mercury_data_800kyr/"
+
+jd = 2453311.5
 
 def orbital_element(textfile):
     rawfile = open(textfile, "rt", encoding = "utf-8")
@@ -30,54 +38,118 @@ def orbital_element(textfile):
                 raw_value.remove('')
             else:
                 continue    
-        print(raw_value)
-        DATA["t"].append(float(raw_value[0]))
-        DATA["a"].append(float(raw_value[3]))
-        DATA["e"].append(float(raw_value[4]))
-        DATA["i"].append(float(raw_value[5]))
-        DATA["Ω"].append(float(raw_value[6]))
-        DATA["ω"].append(float(raw_value[7]))    
+        try:
+            DATA["a"].append(float(raw_value[3]))
+            DATA["e"].append(float(raw_value[4]))
+            DATA["i"].append(float(raw_value[5]))
+            DATA["Ω"].append(float(raw_value[6]))
+            DATA["ω"].append(float(raw_value[7]))
+            DATA["t"].append(float(raw_value[0]) / 1e6)
+        except ValueError:
+            pass   
     
     rawfile.close()
     return DATA
 
-def plot_orbital_element(rawdata):
-    os.chdir("/home/xi-feng/NCU/Meeting/Orbital_Result")
-    orbit_t = rawdata["t"]
-    print("finish loading timescale")
-    orbit_a = rawdata["a"]
-    print("finish loading semimajor axis")
-    orbit_e = rawdata["e"]
-    print("finish loading eccentricity")
-    orbit_i = rawdata["i"]
-    print("finish loading inclination")
-    orbit_Ω = rawdata["Ω"]
-    print("finish loading longtitude of accending node")
-    orbit_ω = rawdata["ω"]
-    print("finish loading argument of perihelion")
-    
-    fig = Mplot.figure(figsize = (40, 10))
-    ax_a, ax_e, ax_i, ax_Om, ax_om = fig.subplots(5, 1)
-    ax_om.set_xlabel("Evolution time (yr)")
+def rec_multi_aeifile():
+    MULTIDATA = {}
+    for aeif in aei_file_list:
+        if aeif == '':
+            continue
+        else:
+            bodyid = aeif.split("/")[-1].replace(".aei", "")
+            MULTIDATA[bodyid] = orbital_element(aeif)
+            print("finish loading", bodyid + classfile)
+    return MULTIDATA        
+
+def plot_orbital_element():
+    fig = Mplot.figure(figsize = (10, 10))
+    ax_a, ax_e, ax_Om, ax_om, ax_i = fig.subplots(5, 1)
+    ax_a.set_title(f"10 clones of Eros's orbital evolution")
+    ax_i.set_xlabel("Evolution time (Myr)")
     ax_a.set_ylabel("a (AU)")
-    ax_a.set_ylim(2.6, 2.8)
+    #ax_a.set_yscale("log")
     ax_e.set_ylabel("e")
     ax_i.set_ylabel("i (deg)")
-    ax_Om.set_ylabel("Ω (deg)")
-    ax_om.set_ylabel("ω (deg)")
-    ax_a.scatter(orbit_t, orbit_a, s = 0.25, c = "black")
+    ax_Om.set_ylabel("q (AU)")
+    ax_om.set_ylabel("Q (AU)")
+    
+    rawdata = rec_multi_aeifile()
+    for rd in rawdata.keys():
+        print(f"case{rd}: {(min(rawdata[rd]['a']), max(rawdata[rd]['a']))}")
+    # saved case
+    # [5, 19, 22, 26, 27, 34, 41, 61, 62, 98]
+    
+    caselist = [list(rawdata.keys())[ID] for ID in [5, 19, 22, 26, 27, 34, 41, 61, 62, 98]]
+    
+    for data in caselist:
+        print("loading", data + classfile, "...")
+        orbit_t = rawdata[data]["t"]
+        print("finish loading timescale", f"{len(orbit_t)} values")
+        orbit_a = rawdata[data]["a"]
+        print("finish loading semimajor axis", f"{len(orbit_a)} values")
+        orbit_e = rawdata[data]["e"]
+        print("finish loading eccentricity", f"{len(orbit_e)} values")
+        orbit_i = rawdata[data]["i"]
+        print("finish loading inclination", f"{len(orbit_i)} values")
+        orbit_Ω = rawdata[data]["Ω"]
+        print("finish loading longtitude of accending inode", f"{len(orbit_Ω)} values")
+        orbit_ω = rawdata[data]["ω"]
+        print("finish loading argument of perihelion", f"{len(orbit_ω)} values") 
+        
+        orbit_q = [orbit_a[i] * (1 - orbit_e[i]) for i in range(len(orbit_a))]
+        orbit_Q = [orbit_a[i] * (1 + orbit_e[i]) for i in range(len(orbit_a))]
+        
+        if data == "000000":
+            continue
+        else:
+            ax_a.plot(orbit_t, orbit_a, linewidth = 1, alpha = 0.75)
+            print("finish scattering 1/5")
+            ax_e.plot(orbit_t, orbit_e, linewidth = 1, alpha = 0.75)
+            print("finish scattering 2/5")
+            ax_i.plot(orbit_t, orbit_i, linewidth = 1, alpha = 0.75)
+            print("finish scattering 3/5")
+            ax_Om.plot(orbit_t, orbit_q, linewidth = 1, alpha = 0.75)
+            print("finish scattering 4/5")
+            ax_om.plot(orbit_t, orbit_Q, linewidth = 1, alpha = 0.75)
+            print("finish scattering 5/5")    
+    
+    init_data = "000000"
+    ax_a.plot(rawdata[init_data]["t"], rawdata[init_data]["a"], color = "black", linewidth = 0.75, alpha = 0.7, label = "original data")
     print("finish scattering 1/5")
-    ax_e.scatter(orbit_t, orbit_e, s = 0.25, c = "black")
+    ax_e.plot(rawdata[init_data]["t"], rawdata[init_data]["e"], color = "black", linewidth = 0.75, alpha = 0.7)
     print("finish scattering 2/5")
-    ax_i.scatter(orbit_t, orbit_i, s = 0.25, c = "black")
+    ax_i.plot(rawdata[init_data]["t"], rawdata[init_data]["i"], color = "black", linewidth = 0.75, alpha = 0.7)
     print("finish scattering 3/5")
-    ax_Om.scatter(orbit_t, orbit_Ω, s = 0.25, c = "black")
+    ax_Om.plot(rawdata[init_data]["t"], [rawdata[init_data]["a"][i] * (1 - rawdata[init_data]["e"][i]) for i in range(len(rawdata[init_data]["a"]))], 
+               color = "black", linewidth = 0.75, alpha = 0.7)
     print("finish scattering 4/5")
-    ax_om.scatter(orbit_t, orbit_ω, s = 0.25, c = "black")
-    print("finish scattering 5/5")
+    ax_om.plot(rawdata[init_data]["t"], [rawdata[init_data]["a"][i] * (1 + rawdata[init_data]["e"][i]) for i in range(len(rawdata[init_data]["a"]))],
+               color = "black", linewidth = 0.75, alpha = 0.7)
+    print("finish scattering 5/5")    
     
+    ax_a.legend()
     Mplot.savefig(f"para_data_mercury_jd{jd}.png")
+    Mplot.close()
     
-plot_orbital_element(orbital_element(dirpath + obj + classfile))
+    figai = Mplot.figure(figsize = (10, 10))
+    axai = figai.add_subplot()
+    axai.set_title("a-i relation of Eros's orbital evolution with 10 clones")
+    axai.set_xlabel("a (AU)")
+    axai.set_ylabel("i (deg)")
+    
+    quartor = int(len(rawdata[init_data]["t"]) / 4)
+    for cp in caselist:
+        axai.scatter([rawdata[cp]["a"][i] for i in range(quartor)], [rawdata[cp]["i"][i] for i in range(quartor)], s = 0.5, c = "blue")
+        axai.scatter([rawdata[cp]["a"][i] for i in range(quartor, 2*quartor)], [rawdata[cp]["i"][i] for i in range(quartor, 2*quartor)], s = 0.5, c = "red")
+        axai.scatter([rawdata[cp]["a"][i] for i in range(2*quartor, 3*quartor)], [rawdata[cp]["i"][i] for i in range(2*quartor, 3*quartor)], 
+                     s = 0.5, c = "orange")
+        axai.scatter([rawdata[cp]["a"][i] for i in range(3*quartor, 4*quartor)], [rawdata[cp]["i"][i] for i in range(3*quartor, 4*quartor)], 
+                     s = 0.5, c = "green")
+    axai.legend(["0 ~ τ/4", "τ/4 ~ τ/2", "τ/2 ~ 3τ/4", "3τ/4 ~ τ"])
+    Mplot.savefig(f"paraAvsI_jd{jd}_mercury.png")
+    Mplot.close()
+    
+plot_orbital_element()
 
 
